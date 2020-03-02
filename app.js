bd524fae2d9b77cae402284b0b8df7d5da4bb941
Nicolas Timm <nntimm1@gmail.com>
2020-03-01 18:32:48 -0600

    test

--- app.js
+++ app.js
@@ -114,21 +114,9 @@ function displayPerson(person){
 
 function displayFamily(person, people){
 
-  let familyInfo = people;
-  
-  
-  
-<<<<<<< HEAD
+  let familyInfo = people;  
   familyInfo = familyInfo.filter(function(el) {
     if(person.parents(contains.id) == people.id) {return true;} else{return false;}
-    // if(el.parents.length > 0) {for (i = 0; i < el.parents.length; i++){return el;}
-=======
-  familyInfo.filter(function(el) {
-    if(el.parents.id == person.parents.id) {return true;} else{return false;}});}
-    
->>>>>>> 0c888b51db546e46ed360e703cfe4aea453acf93
-    // if (el.currentSpouse != null){return el.currentSpouse;}
-    // if (el.child.parents = person.id ){return el;}
 
 
 // function that prompts and validates user input
